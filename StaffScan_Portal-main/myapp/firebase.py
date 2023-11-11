a1ccbe30ec6610d35271efe5342cdcbd0b31f56f
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2023-11-11 21:58:11 +0530

    Delete StaffScan_Portal-main directory

--- StaffScan_Portal-main/myapp/firebase.py
+++
@@ -1,7 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials, db
-
-cred = credentials.Certificate("hackathon2023-4c407-firebase-adminsdk-xqeff-f482eeb1f8.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://hackathon2023-4c407-default-rtdb.firebaseio.com'
-})
