7de788aeb97af4902dfdc7d4277da992d10a2dad
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2023-11-11 09:43:21 +0530

    Intial Commit
    Commit with vercel application

---
+++ StaffScan_Portal-main/myapp/employeeManagement.py
@@ -0,0 +1,211 @@
+from PIL import Image
+import os
+import mysql.connector
+from django.shortcuts import render,HttpResponse
+import mysql.connector
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from django.shortcuts import render
+import csv
+from io import TextIOWrapper
+
+class employeeManagement:
+    """
+    A class for managing employee data in a MySQL database.
+    """
+
+    def __init__(self):
+        """
+        Initializes a new instance of the employeeManagement class.
+        """
+        self.conn = mysql.connector.connect(
+            host="localhost",
+            user="unknown",
+            password="password",
+            database="hackathon"
+        )
+
+    def checkCredentials(self, username, password):
+        """
+        Checks if the given username and password match an admin login in the database.
+
+        Args:
+            username (str): The username to check.
+            password (str): The password to check.
+
+        Returns:
+            bool: True if the credentials match an admin login, False otherwise.
+        """
+        query = "SELECT * FROM admin_login WHERE username = %s AND password = %s"
+        cursor = self.conn.cursor()
+        cursor.execute(query, (username, password))
+        user = cursor.fetchone()
+        return user[0] if user else False
+
+    def saveEmployeeForm(self, form, admin_id):
+        """
+        Saves employee data from a Django form to the database.
+
+        Args:
+            form (EmployeeForm): The form containing the employee data.
+        """
+        print("Going to save Employee data.")
+        
+        name = form.cleaned_data.get('name')
+        mobile = form.cleaned_data.get('mobile_number')
+        email = form.cleaned_data.get('email')
+        password = email
+        education = form.cleaned_data.get('education')
+        position = form.cleaned_data.get('position')
+        salary = form.cleaned_data.get('salary')
+        photo1 = form.cleaned_data.get('faceImage').read()  # Read the binary data from the image file
+
+        try:
+            # Create a cursor
+            cursor = self.conn.cursor()
+
+            # Define the SQL INSERT statement
+            insert_query = "INSERT INTO Employee (Name, MobileNumber, EmailID, password, education, position, salary, faceImage, admin_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)" 
+            # Execute the INSERT statement with the values
+            cursor.execute(insert_query, (name, mobile, email, password, education, position, salary, photo1, admin_id))
+
+            # Commit the transaction
+            self.conn.commit()
+
+            # Close the cursor and connection
+            cursor.close()
+            self.conn.close()
+
+            print("Employee data saved successfully.")
+
+        except mysql.connector.Error as e:
+            print(f"Error: {e}")
+
+    def showAllEmployees(self, admin_id=0):
+        """
+        Retrieves all employee data from the database.
+
+        Returns:
+            list: A list of dictionaries containing employee data.
+        """
+        cursor = self.conn.cursor(dictionary=True)  # Use dictionary cursor to fetch data as dictionaries
+        print(admin_id)
+        # Define your SQL query to fetch employee data
+        sql_query = "SELECT EmployeeID, Name, MobileNumber, EmailID, faceImage FROM Employee where admin_id = " + str(admin_id)
+
+        cursor.execute(sql_query)
+        employees = cursor.fetchall()
+
+        cursor.close()
+        return employees
+
+    def isPresent(self):
+        """
+        Retrieves the IDs of all employees who are currently present at work.
+
+        Returns:
+            list: A list of integers representing the IDs of present employees.
+        """
+        cursor = self.conn.cursor(dictionary=True)
+        sql_query = "SELECT EmployeeID FROM EMPLOYEE_ATTENDANCE WHERE DATE(Start_Time) = CURDATE() AND End_Time IS NULL"
+        cursor.execute(sql_query)
+        present_employees = cursor.fetchall()
+        present_id = []
+        for i in present_employees:
+            present_id.append(i['EmployeeID'])
+        return present_id
+
+    def updateEmployeeForm(self, request):
+        """
+        Updates employee data in the database based on a Django form.
+
+        Args:
+            request (django.http.request.HttpRequest): The HTTP request containing the form data.
+
+        Returns:
+            django.shortcuts.render: A rendered HTML template showing all employees in the database.
+        """
+        if request.method == 'POST':
+            try:
+                # Get the data from the form
+                name = request.POST['name']
+                mobile = request.POST['mobile_number']
+                email = request.POST['email']
+                photo = request.FILES.get('photo')
+                id = request.POST['id']
+
+                # Create a cursor
+                cursor = self.conn.cursor()
+
+                # Define the SQL UPDATE statement
+                update_query = "UPDATE Employee SET name = %s, MobileNumber = %s, EmailID = %s"
+                values = (name, mobile, email)
+
+                if photo:
+                    update_query += ", faceImage = %s"
+                    values = (name, mobile, email, photo.read())
+
+                update_query += " WHERE EmployeeID = " +id
+
+                # Execute the UPDATE statement with the values
+                cursor.execute(update_query, values)
+
+                # Commit the changes to the database
+                self.conn.commit()
+
+                cursor.close()
+                print("done")
+                return render(request, 'showEmployee.html', {'employees': employeeManagement().showAllEmployees()})
+            except mysql.connector.Error as e:
+                return HttpResponse(f"Error: {e}")
+            
+        return render(request, 'update_employee_form.html')
+    
+    def upload_csv(self,request):
+        if request.method == 'POST' and request.FILES['csv_file']:
+            csv_file = request.FILES['csv_file']
+
+            if not csv_file.name.endswith('.csv'):
+                return HttpResponse('Not a CSV file')
+
+            # Assuming you have a MySQL connection (self.conn) established
+
+            try:
+                with TextIOWrapper(csv_file.file, encoding='utf-8') as file:
+                    csv_data = csv.reader(file)
+                    next(csv_data)  # Skip the header row
+
+                    
+                    for row in csv_data:
+                        try:
+                            name, mobile, email, education, position, salary = row
+                            password = email  # Using email as password
+
+                            cursor = self.conn.cursor()
+                            insert_query = "INSERT INTO Employee (Name, MobileNumber, EmailID, password, education, position, salary) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                            cursor.execute(insert_query, (name, mobile, email, password, education, position, salary))
+                            self.conn.commit()
+                            cursor.close()
+                        except Exception:
+                            pass
+
+            except Exception as e:
+                return HttpResponse(f'Error: {e}')
+
+            return HttpResponseRedirect('../showEmployee.html')
+
+        
+
+        return render(request, 'upload_csv.html')
+
+
+    def __del__(self):
+        """
+        Closes the database connection when the employeeManagement instance is destroyed.
+        """
+        self.conn.close()
+
+
+if __name__ == "__main__":
+    employeeManagement().isPresent()
---
+++ StaffScan_Portal-main/user_panel/views.py
@@ -0,0 +1,303 @@
+from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.views.decorators import gzip
+from django.http import StreamingHttpResponse, JsonResponse
+from django.db import connection
+import mysql.connector
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+import cv2
+from django.template import loader
+from django.middleware.csrf import get_token
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from firebase_admin import db
+import firebase_admin
+from firebase_admin import credentials, db
+from django.shortcuts import render, redirect
+from .forms import EmployeeLoginForm
+import mysql.connector
+from django.contrib.auth import authenticate, login
+from django.utils import timezone
+from datetime import date
+import firebase_admin
+from firebase_admin import credentials, db
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+import csv
+
+# Initialize Firebase credentials (you've already provided this)
+cred = credentials.Certificate("hackathon2023-4c407-firebase-adminsdk-xqeff-f482eeb1f8.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://hackathon2023-4c407-default-rtdb.firebaseio.com'
+})
+
+
+def admin_login(request):
+    if request.method == 'POST':
+        form = EmployeeLoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data.get('email')
+            password = form.cleaned_data.get('password')
+
+            conn = mysql.connector.connect(
+                host="localhost",
+                user="unknown",
+                password="password",
+                database="hackathon"
+            )
+
+
+            query = "SELECT * FROM Employee WHERE EmailID = %s AND password = %s"
+            cursor = conn.cursor()
+            cursor.execute(query, (email, password))
+            user = cursor.fetchone()
+            
+
+            if user:
+                employee_id = user[0]
+                print(employee_id)
+                request.session['employee_id'] = employee_id
+                return redirect('employee_dashboard')
+            else:
+                messages.error(request, 'Invalid admin credentials. Please try again.')
+    else:
+        form = EmployeeLoginForm()
+    return render(request, 'user_panel_template/employee_login.html', {'form': form})
+
+
+def punch(request):
+    employee_id = request.session.get('employee_id')
+    current_time = timezone.now()
+    current_date = date.today()
+
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT AttendanceID, DATE(Start_Time) FROM EMPLOYEE_ATTENDANCE WHERE EmployeeID = %s AND DATE(Start_Time) = %s AND End_Time IS NULL", (employee_id, current_date))
+        existing_attendance = cursor.fetchone()
+
+        if request.method == 'POST':
+            if 'punch_in' in request.POST:
+                if not existing_attendance:
+                    # Punch In
+                    cursor.execute("INSERT INTO EMPLOYEE_ATTENDANCE (EmployeeID, Start_Time) VALUES (%s, %s)", (employee_id, current_time))
+                    connection.commit()
+
+            elif 'punch_out' in request.POST:
+                if existing_attendance:
+                    # Punch Out
+                    cursor.execute("UPDATE EMPLOYEE_ATTENDANCE SET End_Time = %s WHERE AttendanceID = %s", (current_time, existing_attendance[0]))
+                    connection.commit()
+
+    return redirect('employee_dashboard')
+
+
+def employee_dashboard(request):
+    current_date = date.today()
+    employee_id = request.session.get('employee_id')
+
+    # Retrieve assigned tasks for the employee from Firebase
+    assigned_tasks_ref = db.reference('tasks/' + str(employee_id))
+    assigned_tasks = assigned_tasks_ref.get()
+
+    # Retrieve leave request status from Firebase
+    leave_request_status_ref = db.reference(f'leave_requests/{employee_id}/status')
+    leave_request_status = leave_request_status_ref.get()
+
+    # Retrieve leave requests from Firebase
+    leave_requests_ref = db.reference(f'leave_requests/{employee_id}')
+    leave_requests = leave_requests_ref.get()
+
+    # Ensure the assigned tasks exist and convert them to a list
+    if assigned_tasks is not None:
+        assigned_tasks = list(assigned_tasks.values())
+    else:
+        assigned_tasks = []
+
+    # Initialize other variables with default values
+    punch_in_count = 0
+    punch_out_count = 0
+    employee_name = ""
+
+    with connection.cursor() as cursor:
+        # Check if the employee has punched in on the current date
+        cursor.execute("SELECT COUNT(*) FROM EMPLOYEE_ATTENDANCE WHERE EmployeeID = %s AND DATE(Start_Time) = %s", (employee_id, current_date))
+        result = cursor.fetchone()
+        if result:
+            punch_in_count = result[0]
+
+        # Check if the employee has punched out on the current date
+        cursor.execute("SELECT COUNT(*) FROM EMPLOYEE_ATTENDANCE WHERE EmployeeID = %s AND DATE(End_Time) = %s", (employee_id, current_date))
+        result = cursor.fetchone()
+        if result:
+            punch_out_count = result[0]
+
+        cursor.execute("SELECT Name FROM Employee WHERE EmployeeID = %s", (employee_id,))
+        result = cursor.fetchone()
+        if result:
+            employee_name = result[0]
+
+    
+    cursor.execute("SELECT admin_id FROM Employee WHERE EmployeeID = %s", (employee_id,))
+    admin_id = cursor.fetchone()[0]
+    return render(request, 'user_panel_template/employee_dashboard.html', {
+        'punch_in_count': punch_in_count,
+        'punch_out_count': punch_out_count,
+        'assigned_tasks': assigned_tasks,
+        'employee_name': employee_name,
+        'leave_request_status': leave_request_status,
+        'leave_requests': leave_requests,
+        'admin_id': admin_id
+    })
+
+
+
+def edit_employee_data(request):
+    if request.method == 'POST':
+        try:
+            # Get the data from the form
+            name = request.POST['name']
+            mobile = request.POST['mobile_number']
+            email = request.POST['email']
+            education = request.POST['education']
+            position = request.POST['position']
+            new_password = request.POST['new_password']  # New password field
+            employee_id = request.session.get('employee_id')
+
+            # Create a MySQL connection
+            conn = mysql.connector.connect(
+                host="localhost",
+                user="unknown",
+                password="password",
+                database="hackathon"
+            )
+
+
+            with conn.cursor() as cursor:
+                # Define the SQL UPDATE statement
+                update_query = "UPDATE Employee SET Name = %s, MobileNumber = %s, EmailID = %s, education = %s, position = %s, password = %s WHERE EmployeeID = %s"
+                values = (name, mobile, email, education, position, new_password, employee_id)  # Include new password
+
+                # Execute the UPDATE statement with the values
+                cursor.execute(update_query, values)
+
+                # Commit the changes to the database
+                conn.commit()
+
+
+            return redirect('employee_dashboard')
+
+        except Exception as e:
+            return HttpResponse(f"Error: {e}")
+
+    # Retrieve the existing employee data for pre-filling the form
+    existing_employee = get_existing_employee_data(request.session.get('employee_id'))
+
+    # Handle GET request or render the update form with pre-filled data
+    return render(request, 'user_panel_template/edit_employee_data.html', {'existing_employee': existing_employee})
+
+
+def get_existing_employee_data(employee_id):
+    try:
+        conn = mysql.connector.connect(
+                host="localhost",
+                user="unknown",
+                password="password",
+                database="hackathon"
+            )
+
+        cursor = conn.cursor(dictionary=True)
+
+        # Retrieve the existing employee data based on the employee ID
+        query = "SELECT Name, MobileNumber, EmailID, education, position, salary FROM Employee WHERE EmployeeID = %s"
+        cursor.execute(query, (employee_id,))
+
+        existing_employee = cursor.fetchone()
+        return existing_employee
+
+    except mysql.connector.Error as e:
+        print(f"Error: {e}")
+        return None
+
+
+
+
+@csrf_exempt
+def leave_request(request):
+    remaining_leave = -1
+
+    if request.method == 'POST':
+        start_date = request.POST.get('start_date')
+        end_date = request.POST.get('end_date')
+        description = request.POST.get('description')
+
+        # Create a reference to your Firebase database node where you want to store leave requests
+        employee_id = request.session.get('employee_id')
+        ref = db.reference(f'leave_requests/{employee_id}')
+
+        # Set the leave request data in the Firebase node with the employee's ID
+        ref.set({
+            'start_date': start_date,
+            'end_date': end_date,
+            'description': description
+        })
+
+        return redirect('employee_dashboard')
+
+    # Retrieve the remaining leave data from the MySQL database
+    try:
+        employee_id = request.session.get('employee_id')
+        conn = mysql.connector.connect(
+            host="localhost",
+            user="unknown",
+            password="password",
+            database="hackathon"
+        )
+
+        cursor = conn.cursor(dictionary=True)
+
+        query = "SELECT remaining_leave FROM Employee WHERE EmployeeID = %s"
+        cursor.execute(query, (employee_id,))
+        leaves = cursor.fetchone()
+
+        if leaves:
+            remaining_leave = leaves['remaining_leave']
+            print(remaining_leave)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    return render(request, 'user_panel_template/ask_leave.html', {'remaining_leave': remaining_leave})
+
+
+
+
+def download_attendance(request):
+    # Get the employee ID from the session
+    employee_id = request.session.get('employee_id')
+
+    if not employee_id:
+        return HttpResponse("Employee ID not found in session")
+
+    # Query the database to retrieve attendance data
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT Start_Time, End_Time, leave_start, leave_end FROM EMPLOYEE_ATTENDANCE WHERE EmployeeID = %s", (employee_id,))
+        attendance_data = cursor.fetchall()
+
+    if not attendance_data:
+        return HttpResponse("No attendance data found for this employee")
+
+    # Create a CSV response
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename="employee_attendance.csv"'
+
+    # Create a CSV writer and write the header row
+    csv_writer = csv.writer(response)
+    csv_writer.writerow(['Start Time', 'End Time', 'Leave Start', 'Leave End', 'Status'])
+
+    # Write the attendance data to the CSV file, indicating "onleave" if applicable
+    for row in attendance_data:
+        start_time, end_time, leave_start, leave_end = row
+        status = "onleave" if leave_start and leave_end else ""
+        csv_writer.writerow([start_time, end_time, leave_start, leave_end, status])
+
+    return response
