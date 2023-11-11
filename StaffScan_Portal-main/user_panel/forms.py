617b68f49507245f36799f59f0541dd52aca3f5c
Hirenlimbad <103926290+hirenlimbad@users.noreply.github.com>
2023-11-11 09:40:24 +0530

    Delete StaffScan_Portal-main directory

--- StaffScan_Portal-main/user_panel/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class EmployeeLoginForm(forms.Form):
-    email = forms.EmailField(max_length=100, widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
