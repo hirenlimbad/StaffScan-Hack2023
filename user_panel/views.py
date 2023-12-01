70a575999127a22ff84d43de131713db1a4f1e48
hirenlimbad <limbadhiren00@gmail.com>
2023-12-01 17:52:43 +0530

    removed unnessory files

--- user_panel/views.py
+++ user_panel/views.py
@@ -33,7 +33,7 @@ import io
 import cv2
 import numpy as np
 
-# Initialize Firebase credentials (you've already provided this)
+# use your own credential file here
 cred = credentials.Certificate("hackathon2023-4c407-firebase-adminsdk-xqeff-f482eeb1f8.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://hackathon2023-4c407-default-rtdb.firebaseio.com'
